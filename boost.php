6fab2ec94e45ca8dfb7444e58addc022b7c800b6
GreenCommunications <alexander.m.rankin@gmail.com>
2016-11-22 13:46:43 -0500

    Update boost.php

--- boost.php
+++ boost.php
@@ -1,1 +1,13 @@
+<?php
+$action = $_GET["action"];
 
+if($action == "uploadImages")
+{
+  $username = $_GET["username"];
+  $password = $_POST["password"];
+  
+  foreach($_FILES[] as $image)
+  {
+  }
+}
+?>
